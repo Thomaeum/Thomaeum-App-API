5a198963f356b9d052bd7fb287aa41c83af1cbb7
isiko <koerner@informatik-ag.net>
2022-03-04 22:35:52 +0100

    Add dummy Request for creating a Profile

--- routes/substitution.js
+++ routes/substitution.js
@@ -6,7 +6,33 @@
 const router = require('express').Router();
 const logger = log4js.getLogger('substitution')
 
-// Requests here
+// TODO this is a placeholder and should be replaced with actuall functionality
+let profiles = []
+
+router.post('/register', (req, res, next) => {
+    let profile = []
+
+    try {
+        for (let i = 0; i < req.body.length; i++) {
+            profile.push({
+                grade: req.body.grade,
+                courseType: req.body.courseType,
+                courseNumber: req.body.courseNumber,
+                subject: req.body.subject,
+                teacher: req.body.teacher
+            })
+        }
+    } catch (e) {
+        res.send(400)
+        return
+    }
+
+    profiles[profiles.length] = profile
+
+    res.json({
+        id: profiles.length-1
+    })
+})
 
 module.exports = router;
 
